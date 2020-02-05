8c40d9c143ba593ddb7bb45f73621833a26c57c5
Leo SUARDI <lsuardi@e3r1p13.42.fr>
2020-02-05 17:29:23 +0100

    eh ben

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -10,4 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+int	ft_nbchiffres(int i)
+{
+	int n;
+
+	n = 0;
+	while (i > 0)
+	{
+		i /= 10;
+		n++;
+	}
+	return(n);
+}
+
+void	ft_putnbr(int nb)
+{
+	int cpt;
+	int index;
+	char tab_ch[10];
+
+	cpt = ft_nbchiffres(nb);
+	index = 1;
+	while (cpt > 1)
+	{
+		index *= 10;
+		cpt--;
+	}
+	cpt--;
+	while (index > 0)
+	{
+		tab_ch[cpt] = (char)(nb / index) + 48;
+		write(1, &tab_ch[cpt], 1);
+		nb %= index;
+		index /= 10;
+		cpt++;
+	}
+}
