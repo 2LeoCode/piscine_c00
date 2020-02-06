bcb9ac997663753102d35ad21d094a6b67b607b1
Leo SUARDI <lsuardi@e3r2p21.42.fr>
2020-02-06 17:24:52 +0100

    gros fils de pute

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -13,14 +13,54 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int ft_check(int t[], int nb)
+int		ft_int_to_char(int i)
 {
-	int i = nb - 1;
+	char c;
+
+	c = (char)i + 48;
+	return (c);
+}
+
+void	ft_print_tab(int tab[], int size)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (i < size)
+	{
+		c = ft_int_to_char(tab[i]);
+		write(1, &c, 1);
+		i++;
+	}
+	write(1, ", ", 2);
+}
+
+void	ft_print_tab_end(int tab[], int size)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (i < size)
+	{
+		c = ft_int_to_char(tab[i]);
+		write(1, &c, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int		ft_check(int t[], int nb)
+{
+	int i;
+
+	i = nb - 1;
 	while (t[i] == 10 - (nb - i))
 	{
 		i--;
 	}
-	return(i);
+	return (i);
 }
 
 void	ft_print_combn(int n)
@@ -35,11 +75,7 @@ void	ft_print_combn(int n)
 		a++;
 	}
 	a = n - 1;
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d", tab[i]);
-	}
-	putchar(' ');
+	ft_print_tab(tab, n);
 	while (tab[0] < 10 - n)
 	{
 		a = ft_check(tab, n);
@@ -48,19 +84,11 @@ void	ft_print_combn(int n)
 			while (tab[a] < 9)
 			{
 				tab[a]++;
-				for (int i = 0; i < n; i++)
-				{
-					printf("%d", tab[i]);
-				}
-				putchar(' ');
+				ft_print_tab(tab, n);
 			}
-			tab[a-1]++;
+			tab[a - 1]++;
 			tab[a] = tab[a - 1] + 1;
-			for (int i = 0; i < n; i++)
-			{
-				printf("%d", tab[i]);
-			}
-			putchar(' ');
+			ft_print_tab(tab, n);
 			a = ft_check(tab, n);
 		}
 		else
@@ -71,22 +99,8 @@ void	ft_print_combn(int n)
 				a++;
 				tab[a] = tab[a - 1] + 1;
 			}
-			for (int i = 0; i < n; i++)
-			{
-				printf("%d", tab[i]);
-			}
-			putchar(' ');
+			ft_print_tab(tab, n);
 		}
 	}
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d", tab[i]);
-	}
-	putchar('.');
-}
-
-int main()
-{
-	ft_print_combn(9);
-	return(0);
+	ft_print_tab_end(tab, n);
 }
