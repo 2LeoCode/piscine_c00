b1afc52806d98dde3959c4153f7a8da419378561
Your Name <you@example.co>
2020-02-11 16:25:31 +0100

    ex8 challah

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,26 +6,29 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 17:32:19 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/02/09 18:35:33 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/02/11 15:33:13 by lsuardi          ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************* */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_tab(int tab[], int size)
+void	ft_print_tab(int tab[], int size, int z)
 {
 	int		i;
 	char	c;
 
 	i = 0;
-	while (i < size)
+	if (z == 1 || size != 1)
 	{
-		c = (char)tab[i] + 48;
-		write(1, &c, 1);
-		i++;
+		while (i < size)
+		{
+			c = (char)tab[i] + 48;
+			write(1, &c, 1);
+			i++;
+		}
+		if (tab[0] < 10 - size)
+			write(1, ", ", 2);
 	}
-	if (tab[0] < 10 - size)
-		write(1, ", ", 2);
 }
 
 int		*ft_init(int *tab, int s)
@@ -46,11 +49,15 @@ int		*ft_last_case(int *tab, int s)
 	while (tab[s - 1] < 9)
 	{
 		tab[s - 1]++;
-		ft_print_tab(tab, s);
+		ft_print_tab(tab, s, 1);
 	}
-	tab[s - 2]++;
-	tab[s - 1] = tab[s - 2] + 1;
-	ft_print_tab(tab, s);
+	if (s > 2)
+	{
+		tab[s - 2]++;
+		tab[s - 1] = tab[s - 2] + 1;
+	}
+	if (s != 1)
+		ft_print_tab(tab, s, 1);
 	return (tab);
 }
 
@@ -71,9 +78,11 @@ void	ft_print_combn(int n)
 	int tab[100];
 
 	ft_init(tab, n);
+	if (n == 1)
+		ft_last_case(tab, n);
 	tab[n] = n - 1;
-	ft_print_tab(tab, n);
-	while (tab[0] < 10 - n)
+	ft_print_tab(tab, n, 0);
+	while (tab[0] < 10 - n && n != 1)
 	{
 		tab[n] = ft_check(tab, n);
 		if (tab[n] == n - 1)
@@ -84,19 +93,11 @@ void	ft_print_combn(int n)
 		else
 		{
 			tab[tab[n]]++;
-			while (tab[n] < n)
+			while (++tab[n] <= n)
 			{
-				tab[n]++;
 				tab[tab[n]] = tab[tab[n] - 1] + 1;
 			}
-			ft_print_tab(tab, n);
+			ft_print_tab(tab, n, 1);
 		}
 	}
-}
-
-int main ()
-
-{
-	ft_print_combn(5);
-	return 0;
 }
